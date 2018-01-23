d585b37899001f98842218f5b0625c1ecdf9802e
hvdson <hvdsonmusic@gmail.com>
2018-01-23 04:47:36 +0000

    hid auth token

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,22 @@
 var request = require('request');
-
+var accessToken = require("./secrets").GITHUB_TOKEN;
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url:  "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      "User-Agent": "request",
+      "Authorization": accessToken
+    }
+  };
+
+  request(options, (err, res, body) => {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", (err, result) => {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
